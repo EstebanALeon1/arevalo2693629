0ff846c12218a02487d15c15bba562b1b260babf
esteban <estebanarevalo2017@gmail.com>
2023-04-26 13:51:18 -0500

    ciclo for1 con comentarios

---
+++ TAREAS/TAREA2 COMENTARIOS CODIGO INSTRUCTOR/bucles/for1.py
@@ -0,0 +1,22 @@
+for i in range(11): #uso del ciclo for con un argumento marcando como 11 el final del ciclo
+    print(i) #imprime los numeros de 0 al 10
+    
+for j in range(1,11): #uso del ciclo for con dos argumentos siendo el primero el inicio del bucle y el segundo su final 
+    print(j) #imprime los numeros de 1 al 10
+
+
+for k in range(0,11,2): #uso del ciclo for con tres argumentos siendo el primero el inicio del bucle, el segundo su final y el tercero hace que i cuente cada n determinada de veces 
+    print(i) #imprime los numeros de 0 a 10 pero esta vez de dos en dos 
+    
+    
+for i in range(10,-1,-2): #uso del ciclo for con 3 argumentos este se usa para invertir el orden de la lista 
+    print(i) #imprime del 10 al 0 de dos en dos 
+    
+    
+for i in range(11): #uso del ciclo for con un argumento
+    print("amo programar en python") #imprime el la pantalla amor programar en python el numero de veces que contenga i
+    
+    
+for i in range(1,11): #uso del ciclo for 
+    print(i**2) # imprime en pantalla la exponenciacion de cada numero en el que el ciclo for ejecute i
+    
