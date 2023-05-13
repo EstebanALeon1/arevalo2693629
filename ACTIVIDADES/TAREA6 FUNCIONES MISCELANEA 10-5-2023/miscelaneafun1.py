c385b9d6081a46aabfa8599962a6abbcdf90b496
esteban <estebanarevalo2017@gmail.com>
2023-05-12 19:04:32 -0500

    actividad de funciones

--- ACTIVIDADES/TAREA6 FUNCIONES MISCELANEA 10-5-2023/miscelaneafun1.py
+++ ACTIVIDADES/TAREA6 FUNCIONES MISCELANEA 10-5-2023/miscelaneafun1.py
@@ -116,3 +116,17 @@ def Mediana(lista):
         mediana = lista[(len(lista)//2)]
         print(f"La mediana de la lista es {mediana}")
 Mediana(lista_1)
+
+
+#j. Buscar. Decir si encuentra el número, en qué posición(es) está, cuantas veces está
+def BuscarNumero(lista):
+    num_buscar = int(input("Digite el numero que desea buscar: "))
+    contador = 0
+    for i in range(len(lista)):
+        contador = 0
+        for j in range(len(lista)):
+            if lista[i] == lista[j]:
+                if lista[i] == num_buscar:
+                    contador += 1
+                print(f"el numero que desea buscar esta en la poscicion [{i}] y se repite {contador} veces")
+BuscarNumero(lista_1)
